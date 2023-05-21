073f123e474e909635bf58723d83aad665788058
Daniel07Navarro <Daniel7Navarro7@gmail.com>
2023-05-21 18:04:28 -0500

    validando formulario registro

--- src/app/components/login-body/login-body.component.ts
+++ src/app/components/login-body/login-body.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+import { RegistroService } from 'src/app/services/registro.service';
 
 @Component({
   selector: 'app-login-body',
@@ -9,11 +11,32 @@ import { Component } from '@angular/core';
 export class LoginBodyComponent {
   optionSelected?: string;
 
-  constructor() {
+  constructor(private serviceRegistro:RegistroService) {
     this.optionSelected = "login";
   }
 
   onOptionClick(option: string) {
     this.optionSelected = option; 
   }
+
+  cliente:Cliente={
+    idCliente:0,
+    nombreCompleto:"",
+    apellido:"",
+    telefono:"",
+    dni:"",
+    email:"",
+    direccion:"",
+    password:"",
+  };
+
+  registrarse(){
+    this.serviceRegistro.registrarCliente(this.cliente).subscribe((data:Cliente) =>{
+      this.cliente=data;
+    },(error)=>{
+      console.log(error)
+    })
+    alert("REGISTRO EXITOSO");
+  }
+
 }
---
+++ src/app/models/cliente.ts
@@ -0,0 +1,10 @@
+export class Cliente {
+    idCliente!:number;
+    nombreCompleto!:string;
+    apellido!:string;
+    telefono!:string;
+    dni!:string;
+    email!:string;
+    direccion!:string;
+    password!:string
+}
