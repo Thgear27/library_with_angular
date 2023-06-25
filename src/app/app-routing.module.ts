95d86704574600c53a79bd951835b6ccd7b5cd14
Fernando Vegas <vegasfernando2003@gmail.com>
2023-06-24 19:47:42 -0500

    estilos hechos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './views/home/home.component';
 import { LoginRegisterComponent } from './views/login-register/login-register.component';
 import { SearchComponent } from './views/search/search.component';
 import { DetailsComponent } from './views/details/details.component';
+import { CarritoComponent } from './views/carrito/carrito.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'search', component: SearchComponent}, 
   { path: 'login-register', component: LoginRegisterComponent },
   { path: 'booksdetails/:id', component: DetailsComponent},
+  { path: 'carrito', component: CarritoComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/carrito-body/carrito-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito-body',
+  templateUrl: './carrito-body.component.html',
+  styleUrls: ['./carrito-body.component.scss']
+})
+export class CarritoBodyComponent {
+
+}
